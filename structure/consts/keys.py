8a9eefe728fc5298499bf16d74d366326ccd0555
Junius Pun <juniuspun00@gmail.com>
2024-07-23 11:47:35 -0400

    chore: comment out misleading keys

--- structure/consts/keys.py
+++ structure/consts/keys.py
@@ -6,6 +6,6 @@ KEY_EXP = "exp"                 #indicate Experiment params
 KEY_ARCH = "arch"               #indicate ArchFactory params
 
 KEY_DESIGN = "design"           #indicate Design params
-KEY_DESIGN_SDC = "sdc"          #indicate params to be fed into gen_sdc()
-KEY_DESIGN_TCL = "tcl"          #indicate params to be fed into gen_tcl()
-KEY_DESIGN_WRAPPER = "wrapper"  #indicate params to be fed into gen_wrapper()
+# KEY_DESIGN_SDC = "sdc"          #indicate params to be fed into gen_sdc()
+# KEY_DESIGN_TCL = "tcl"          #indicate params to be fed into gen_tcl()
+# KEY_DESIGN_WRAPPER = "wrapper"  #indicate params to be fed into gen_wrapper()
