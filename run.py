386647bd09b9768e20acfc06a4875c1d27fbfb9d
Junius Pun <juniuspun00@gmail.com>
2024-11-22 17:23:41 -0500

    chore: update runner to LUT Skip 6

--- run.py
+++ run.py
@@ -5,6 +5,7 @@ from runs.vtr_denoised_v1 import run_vtr_denoised_v1
 import runs.benchmarks as general_bm
 import runs.benchmarks.kratos as kratos
 import runs.benchmarks.kratos_mini as mini
+import runs.benchmarks.kratos_tiny as tiny
 
 import util.derived_metrics as derived_metrics
 
@@ -14,6 +15,8 @@ from impl.arch.stratix_IV.gen_exp_fpop import GenExpFpopArchFactory
 # Stratix 10
 from impl.arch.stratix_10.base import BaseArchFactory
 from impl.arch.stratix_10.lut_skip import LUTSkipArchFactory
+from impl.arch.stratix_10.lut_skip_1d import LUTSkip1dArchFactory
+from impl.arch.stratix_10.lut_skip_6 import LUTSkip6ArchFactory
 
 # Conv-1D
 from impl.design.conv_1d.fu import Conv1dFuDesign
@@ -23,6 +26,7 @@ from impl.design.conv_1d.pw import Conv1dPwDesign
 from impl.design.conv_2d.fu import Conv2dFuDesign
 from impl.design.conv_2d.rp import Conv2dRpDesign
 from impl.design.conv_2d.pw import Conv2dPwDesign
+from impl.design.conv_2d.stress_test.fu_and_shaxN import Conv2dFuAndShaxNDesign
 
 # GEMM-T
 from impl.design.gemmt.fu import GemmTFuDesign
@@ -45,17 +49,22 @@ BASE_PARAMS = {
         'adder_cin_global': False,
         'soft_multiplier_adders': True,
         'route_chan_width': 400,
-        'force_denser_packing': True,
+        # 'force_denser_packing': True,
         # ... additional Experiment.run() parameters
     },
     keys.KEY_ARCH: {
         # fixed architecture parameters for both baseline and explored
         'cin_mux_stride': 0,
-        'direct_ff_mux_with': 'adder',
+        # 'enable_lut6': False,
+        # 'direct_ff_mux_with': 'adder',
     },
     keys.KEY_DESIGN: {
         'sparsity': [0, 0.5, 0.9],
+        # 'sparsity': 0,
+        # 'data_width': 6,
+        # 'sha_num': list(range(2, 31, 2)),
         'data_width': list(range(3, 9)), # 3-8
+        # 'sha_num': list(range(2, 11, 2)),
     }
 }
 
@@ -64,6 +73,16 @@ VARIABLE_ARCH_PARAMS = dict(
 )
 
 DESIGN_LIST = [
+    # Tiny benchmarks
+    # (Conv1dFuDesign(), tiny.get_conv_1d_fu_params(BASE_PARAMS)),
+    # (Conv1dPwDesign(), tiny.get_conv_1d_pw_params(BASE_PARAMS)),
+    # (Conv2dFuDesign(), tiny.get_conv_2d_fu_params(BASE_PARAMS)),
+    # (Conv2dRpDesign(), tiny.get_conv_2d_rp_params(BASE_PARAMS)),
+    # (Conv2dPwDesign(), tiny.get_conv_2d_pw_params(BASE_PARAMS)),
+    # (GemmTFuDesign(), tiny.get_gemmt_fu_params(BASE_PARAMS)),
+    # (GemmTRpDesign(), tiny.get_gemmt_rp_params(BASE_PARAMS)),
+    # (GemmSDesign(), tiny.get_gemms_params(BASE_PARAMS)),
+
     # Mini benchmarks
     # (Conv1dFuDesign(), mini.get_conv_1d_fu_params(BASE_PARAMS)),
     # (Conv1dPwDesign(), mini.get_conv_1d_pw_params(BASE_PARAMS)),
@@ -75,15 +94,17 @@ DESIGN_LIST = [
     # (GemmSDesign(), mini.get_gemms_params(BASE_PARAMS)),
 
     # Kratos benchmarks
-    # (Conv1dFuDesign(), kratos.get_conv_1d_fu_params(BASE_PARAMS)),
+    (Conv1dFuDesign(), kratos.get_conv_1d_fu_params(BASE_PARAMS)),
     (Conv1dPwDesign(), kratos.get_conv_1d_pw_params(BASE_PARAMS)),
     (Conv2dFuDesign(), kratos.get_conv_2d_fu_params(BASE_PARAMS)),
     # (Conv2dRpDesign(), kratos.get_conv_2d_rp_params(BASE_PARAMS)),
     # (Conv2dPwDesign(), kratos.get_conv_2d_pw_params(BASE_PARAMS)),
     # (GemmTFuDesign(), kratos.get_gemmt_fu_params(BASE_PARAMS)),
-    (GemmTRpDesign(), kratos.get_gemmt_rp_params(BASE_PARAMS)),
-    (GemmSDesign(), kratos.get_gemms_params(BASE_PARAMS)),
+    # (GemmTRpDesign(), kratos.get_gemmt_rp_params(BASE_PARAMS)),
+    # (GemmSDesign(), kratos.get_gemms_params(BASE_PARAMS)),
 
+    # Stress tests
+    # (Conv2dFuAndShaxNDesign(), kratos.get_conv_2d_fu_and_shaxN_params(BASE_PARAMS, exp_root_dir='conv_2d/fu_and_shaxN'))
     # Benchmarks for verification
     # (SimpleUnrolledDesign(), general_bm.get_params(BASE_PARAMS, 'simple_unrolled', {}))
 ]
@@ -99,6 +120,14 @@ def add_derived_metrics(df: DataFrame) -> tuple[DataFrame, list[str]]:
     df = derived_metrics.apply_lut5_to_adder_ratio(df)
     df = derived_metrics.apply_lut5_concurrency(df)
 
+    # 5-LUT measurements
+    df = derived_metrics.apply_lut6_to_adder_ratio(df)
+    df = derived_metrics.apply_lut6_concurrency(df)
+
+    # 5/6-LUT measurements
+    df = derived_metrics.apply_lut56_to_adder_ratio(df)
+    df = derived_metrics.apply_lut56_concurrency(df)
+    
     # Area calculations
     df = derived_metrics.apply_area_fle(df)
 
@@ -111,43 +140,50 @@ def add_derived_metrics(df: DataFrame) -> tuple[DataFrame, list[str]]:
         'adder_avg_util',
         'lut5/adder',
         'lut5_concurrency',
+        'lut6/adder',
+        'lut6_concurrency',
+        'lut56/adder',
+        'lut56_concurrency',
         'area_fle',
         # 'adp_used',
         'adp_fle', 
     ]
 
 run_vtr_denoised_v1(
-    new_arch=LUTSkipArchFactory,
+    # new_arch=LUTSkipArchFactory,
+    # new_arch=LUTSkip1dArchFactory,
+    new_arch=LUTSkip6ArchFactory,
     base_arch=BaseArchFactory,
     design_list=DESIGN_LIST,
     variable_arch_params=VARIABLE_ARCH_PARAMS,
+    # x_axis=['sha_num'],
     x_axis=['data_width'],
-    filter_params_baseline=['data_width', 'sparsity'],
+    filter_params_baseline=[
+        # 'sha_num', 
+        'data_width', 'sparsity'],
     filter_params_add=['per_fle_area'],
+    # group_cols=['data_width'],
     group_cols=['sparsity'],
-    group_cols_short_labels=dict(sparsity='s'),
+    group_cols_short_labels=dict(sparsity='s', data_width='dw'),
     filter_results=['fmax', 'cpd', 'twl', 
-                    'concurrent_lut5s',
-                    # 'area_total_used'
+                    'concurrent_lut5s', 'concurrent_lut6s',
                     ],
     filter_blocks=['clb', 'fle',
-                    'lut5',
-                    'dual_lut4s.lut5',
-                    # 'flut5.lut5',
+                    'lut5', 'lut6',
                     'adder',
                    ],
-    avoid_norm=[ 
-                # 'lut5',
-                # 'dual_lut4s.lut5',
-                # 'flut5.lut5',
+    avoid_norm=[
                 'lut5/adder',
                 'lut5_concurrency',
+                'lut6/adder',
+                'lut6_concurrency',
+                'lut56/adder',
+                'lut56_concurrency',
                 ],
     avoid_plot=[
-                'concurrent_lut5s',
                 'per_fle_area',
-                'lut5',
-                'dual_lut4s.lut5',
+                'concurrent_lut5s', 'concurrent_lut6s',
+                'lut5', 'lut6',
                 'adder',
                 ],
     translations=TRANSLATIONS_GRAPH,
@@ -155,4 +191,7 @@ run_vtr_denoised_v1(
     merge_designs=True,
     num_parallel_tasks=4,
     verbose=True,
+    desc='Base vs. LUT Skip[6], conv1d-FU/PW, conv2d-FU',
+    tele_batch=25,
+    # notify_via_tele=False,
 )
