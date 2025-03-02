7820c1c92e4eecd6e9ae6561b7af2ec0cbf63f55
Junius Pun <juniuspun00@gmail.com>
2025-03-02 08:15:26 -0500

    fix: threshold-crossing memory monitor

--- memory_monitor.py
+++ memory_monitor.py
@@ -6,14 +6,19 @@ import psutil as psu
 from time import sleep
 from util.external_notifs import notification_backend
 
-MACHINE_NAME = "machine" # enter machine name
+MACHINE_NAME = "orca" # enter machine name
 NOTIFY_METHOD = 'telegram' # change to notification service
-USE_THRESHOLD = 0.95 # alert threshold
+USE_THRESHOLD = 0.98 # alert threshold
 INTERVAL_S = 30
 
+threshold_crossed = False
 while True:
     ram_info = psu.virtual_memory()
     ram_usage = ram_info.used / ram_info.total
     if ram_usage > USE_THRESHOLD:
-        notification_backend(NOTIFY_METHOD, f"!!!WARNING!!! RAM usage for '{MACHINE_NAME}' at {ram_usage*100:.2f}%!")
+        if not threshold_crossed:
+            notification_backend(NOTIFY_METHOD, f"!!!WARNING!!! RAM usage for '{MACHINE_NAME}' at {ram_usage*100:.2f}%!")
+            threshold_crossed = True
+    elif threshold_crossed:
+        notification_backend(NOTIFY_METHOD, f"RAM usage for '{MACHINE_NAME}' has returned below threshold ({ram_usage*100:.2f}%).")
     sleep(INTERVAL_S)
