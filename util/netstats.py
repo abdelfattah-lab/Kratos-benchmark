0ba12cba17090843f533dcea910c749d65f2c26b
Junius Pun <juniuspun00@gmail.com>
2025-03-27 23:52:16 +0800

    fix: check for open

--- util/netstats.py
+++ util/netstats.py
@@ -15,6 +15,9 @@ def check_element_is_wire(elem: Element) -> bool:
     """
     Check that @mode == 'wire'.
     """
+    if not check_element_not_open(elem):
+        return False
+
     return elem.attrib['mode'] == 'wire'
 
 def find_all_block_instances(root: Element, instance_name: str) -> list[Element]:
